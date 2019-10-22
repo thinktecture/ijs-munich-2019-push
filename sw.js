04bebfb50f8c94984c1a5a80d57f4d1c12836dd9
Christian Liebel <christian.liebel@thinktecture.com>
2019-10-22 13:50:34 +0200

    demo(4): add push registration

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
 self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('push', event => {
+    const notification = event.data.json();
+    self.registration.showNotification(notification.title, notification);
+});
